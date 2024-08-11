acc10581b7b903bd951230db8f946136a70faceb
hiennv <hiennv@ghn.vn>
2024-08-12 00:17:08 +0700

    commit

--- tests/FirstContract.spec.ts
+++ tests/FirstContract.spec.ts
@@ -5,13 +5,12 @@ import '@ton/test-utils';
 
 describe('FirstContract', () => {
     let blockchain: Blockchain;
-    let deployer: SandboxContract<TreasuryContract>;
     let firstContract: SandboxContract<FirstContract>;
-
+    let deployer: SandboxContract<TreasuryContract>
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        firstContract = blockchain.openContract(await FirstContract.fromInit());
+        firstContract = blockchain.openContract(await FirstContract.fromInit(1000n));
 
         deployer = await blockchain.treasury('deployer');
 
@@ -38,4 +37,49 @@ describe('FirstContract', () => {
         // the check is done inside beforeEach
         // blockchain and firstContract are ready to use
     });
+
+    it("should increase", async ()=>{
+        const counterBefore = await firstContract.getCounter()
+
+        console.log("counterBefore - ", counterBefore)
+
+        await firstContract.send(
+            deployer.getSender(),
+            {
+                value: toNano("0.02")
+            },
+            'increment'
+        )
+
+        const counterAfter = await firstContract.getCounter()
+
+        console.log("counterAfter - ", counterAfter)
+
+        expect(counterBefore).toBeLessThan(counterAfter)
+    })
+
+    it("should increase with amount", async ()=>{
+        const counterBefore = await firstContract.getCounter()
+
+        const amount = 5n;
+
+        console.log("counterBefore - ", counterBefore)
+
+        await firstContract.send(
+            deployer.getSender(),
+            {
+                value: toNano("0.02")
+            },
+            {
+                $$type: 'Add',
+                amount: amount
+            }
+        )
+
+        const counterAfter = await firstContract.getCounter()
+
+        console.log("counterAfter - ", counterAfter)
+
+        expect(counterBefore).toBeLessThan(counterAfter)
+    })
 });
